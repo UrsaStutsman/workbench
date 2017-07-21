0d7703f6c5d7c45acb679f651be3b04de3aa6817
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2017-07-21 17:35:20 -0500

    Debug

--- api/generate_appengine_web_xml.sh
+++ api/generate_appengine_web_xml.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -e
+echo "DB CONNECTION = ${DB_CONNECTION_STRING}"
 cat src/main/webapp/WEB-INF/appengine-web.xml.template | envsubst > src/main/webapp/WEB-INF/appengine-web.xml
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+echo "DB 1 = ${DB_CONNECTION_STRING}"
 ./ci/activate_creds.sh ~/gcloud-credentials.key
 ./tools/deploy.py \
   --target $1 \
