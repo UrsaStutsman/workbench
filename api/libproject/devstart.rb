79d1a53e5f9ce5b76a56792d5521e418d5df8c47
David Mohs <dgit@mohsinc.com>
2017-08-17 15:11:05 -0400

    Checkpoint

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -10,6 +10,15 @@ def dev_up()
   common.run_inline_swallowing_interrupt %W{docker-compose logs -f api}
 end
 
+def restart_dev_server()
+  common = Common.new
+
+  cmd = "kill -HUP $(ps -o pid,comm | grep java | tail -n 1 | awk -F '[ ]+' -e '{print $2}')"
+  common.run_inline %W{
+    docker-compose exec api sh -c #{cmd}
+  }
+end
+
 def rebuild_image()
   common = Common.new
   common.docker.requires_docker
@@ -21,6 +30,13 @@ Common.register_command({
   :invocation => "dev-up",
   :description => "Brings up the development environment.",
   :fn => Proc.new { |*args| dev_up(*args) }
+})
+
+Common.register_command({
+  :invocation => "restart-dev-server",
+  :description =>
+    "Restarts the dev server within the docker container (e.g., after changing source code).",
+  :fn => Proc.new { |*args| restart_dev_server(*args) }
 })
 
 Common.register_command({
--- api/src/main/java/org/pmiops/workbench/config/CommonConfig.java
+++ api/src/main/java/org/pmiops/workbench/config/CommonConfig.java
@@ -4,9 +4,15 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.apache.ApacheHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.Clock;
+import java.util.Map;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 
 @Configuration
 public class CommonConfig {
@@ -23,4 +29,36 @@ public class CommonConfig {
 
   @Bean
   Clock clock() { return Clock.systemUTC(); }
+
+  @Bean
+  DataSource dataSource() {
+    Map<String, String> env = System.getenv();
+    for (String envName : env.keySet()) {
+      System.out.format("%s=%s%n", envName, env.get(envName));
+    }
+    if (env.get("invalid-environment-variable-name") == null) {
+      System.err.println("Environment variable spring.datasource.url required");
+      return null;
+    }
+    DataSource ds = DataSourceBuilder.create().build();
+    Connection c = null;
+    try {
+      c = ds.getConnection();
+    } catch (SQLException e) {
+      System.err.format("Failed to establish database connection: %s%n", e);
+      return null;
+    }
+    PreparedStatement ps = null;
+    try {
+      ps = c.prepareStatement("select true;");
+    } catch (SQLException e) {
+      System.err.format("Failed to prepare test SQL statement: %s", e);
+    }
+    try {
+      ps.execute();
+    } catch (SQLException e) {
+      System.err.format("Failed to execute test SQL statement: %s", e);
+    }
+    return ds;
+  }
 }
